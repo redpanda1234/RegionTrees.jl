baf5c7233a90193c32c9aa413b1d382b7a3b0cbf
redpanda1234 <fkobayashi@hmc.edu>
2024-10-29 14:30:29 -0700

    remove unnecessary fields

--- src/cell.jl
+++ src/cell.jl
@@ -4,8 +4,8 @@ mutable struct Cell{Data, N, T, L}
     divisions::SVector{N, T}
     children::Union{TwosArray{N, Cell{Data, N, T, L}, L}, Nothing}
     parent::Union{Cell{Data, N, T, L}, Nothing}
-    leaves::Union{Set{Int64}, Nothing}
-    ancestors::Union{Dict{Int64, Cell{Data, N, T, L}}, Nothing}
+    # leaves::Union{Set{Int64}, Nothing}
+    # ancestors::Union{Dict{Int64, Cell{Data, N, T, L}}, Nothing}
     diam::Union{Float64, Nothing}
     index::Union{Int64, Nothing}
 end
@@ -24,8 +24,8 @@ end
              boundary.origin + boundary.widths / 2,
              nothing,
              nothing,
-             nothing,
-             nothing,
+             # nothing,
+             # nothing,
              norm(boundary.widths),
              nothing)
     end
